ad320223aa92b3fd7f577511812470af2a13f829
gfclassb <3481522072@qq.com>
2026-01-01 18:49:25 +0800

    test

---
+++ src/client/java/omori_sunny/waterutils/WaterUtilsClient.java
@@ -0,0 +1,10 @@
+package omori_sunny.waterutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WaterUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
